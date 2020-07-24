33c5d7094f550d15d041b0c94010cf9bdca40596
Douglas Arigah <56113272+darigah@users.noreply.github.com>
2020-07-24 15:19:07 +0300

    updating fodder
    updating fodder

---
+++ model/fodder.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var fodderSchema = new Schema({
+    fodderType: String,
+    bucketsNumber: Number,
+    entryDate: Date,
+    entryTime: Time,
+
+});
+
+let Fodderentry = mongoose.model('Fodderentry', fodderSchema);
+
+module.exports = Fodderentry;
